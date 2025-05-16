40c2f0070737589e2e6bf176c734af7986a188eb
nususu1004 <nususu1004@gmail.com>
2025-05-16 13:19:50 +0900

    20250516

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,27 @@
-const say = function () {};
-const cry = () => {};
+// 데이터 서버에 자료를 호출함.
 
-// 매개변수로 전달된 함수 실행
-function run(a) {
-  a();
+function getData(api = "posts") {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://jsonplaceholder.typicode.com/${api}`);
+    xhr.send();
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // 성공
+        resolve(xhr.response);
+      } else if (xhr.status === 404) {
+        // 실패
+        reject();
+      } else if (xhr.status === 505) {
+        console.log("서버가 불안정합니다. 잠시 후 재접속해주세요.");
+      }
+    };
+  });
 }
-
-run(say);
-run(cry);
+// 함수 사용
+getData("posts").then().catch();
+getData("comments").then().catch();
+getData("albums").then().catch();
+getData("photos").then().catch();
+getData("todos").then().catch();
+getData("users").then().catch();
